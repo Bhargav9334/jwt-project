546344484f3b761fe220875459d285085e2a34c3
Bhargav9334 <bhargavsinha93@gmail.com>
2026-05-25 17:10:27 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,26 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: " https://jwt-backend-8rxw.onrender.com"
+  baseURL:
+    "https://jwt-backend-8rxw.onrender.com/api"
 });
 
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
+API.interceptors.request.use(
+  (req) => {
 
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
+    const token =
+      localStorage.getItem(
+        "token"
+      );
+
+    if (token) {
+
+      req.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return req;
   }
-
-  return req;
-});
+);
 
 export default API;
